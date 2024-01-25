78413a47784eaca7b6c5d7fb06f6033f9ec0b98f
Druhin Tarafder <druhinpro@gmail.com>
2024-01-25 00:13:26 +0000

    test

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,34 +1,29 @@
-$(document).ready(function () {
-    // Function to open the popup
-    function openPopup() {
-        $('.popup-overlay').fadeIn();
-    }
+$(document).ready(function() {
+  // Function to open the popup by triggering the animation
+  function openPopup() {
+    $('.popup-open-anim').click();
+  }
 
-    // Function to close the popup
-    function closePopup() {
-        $('.popup-overlay').fadeOut();
-    }
+  // Function to close the popup by triggering the animation
+  function closePopup() {
+    $('.popup-close-anim').click();
+  }
 
-    // Open the popup after 10 seconds on all pages except the contact page
-    const currentPage = window.location.pathname;
-    if (currentPage !== '/contact') {
-        setTimeout(openPopup, 10000); // 10 seconds delay
-    }
+  // Open the popup after 10 seconds on all pages except the contact page
+  const currentPage = window.location.pathname;
+  if (currentPage !== '/contact') {
+    setTimeout(openPopup, 10000); // 10 seconds delay
+  }
 
-    // Close the popup when the close button is clicked
-    $('#popup-close-button, #popup-close').on('click', function () {
-        closePopup();
-    });
+  // Permanently close the popup when the "Don't show this again" link is clicked
+  $('#popup-permanent-close').on('click', function() {
+    closePopup();
+    // Set a localStorage flag to remember the user's preference
+    localStorage.setItem('popupClosed', 'true');
+  });
 
-    // Permanently close the popup when the "Don't show this again" link is clicked
-    $('#popup-permanent-close').on('click', function () {
-        closePopup();
-        // Set a localStorage flag to remember the user's preference
-        localStorage.setItem('popupClosed', 'true');
-    });
-
-    // Check if the user has already closed the popup permanently and prevent it from showing
-    if (localStorage.getItem('popupClosed') === 'true') {
-        $('.popup-overlay').hide();
-    }
+  // Check if the user has already closed the popup permanently and prevent it from showing
+  if (localStorage.getItem('popupClosed') === 'true') {
+    closePopup();
+  }
 });
